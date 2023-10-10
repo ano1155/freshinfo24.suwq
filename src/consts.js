72a951ab1b9e80c5b4709af208146745a159ab9f
ano1155 <111393049+ano1155@users.noreply.github.com>
2023-10-10 23:23:23 +0400

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,2 +1,2 @@
-export const	HOST = 'https://majestic-animals.su/';
-export const	DOMAIN = 'https://majestic-animals-su.pages.dev/';
+export const	HOST = 'https://freshinfo24.su/';
+export const	DOMAIN = 'https://freshinfo-24-su.pages.dev/';
